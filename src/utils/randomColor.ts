3c2eb32e41f564bd6ac9bed5744eaa38380f1a32
Michael Braun <michael@web4brauns.de>
2021-09-22 23:01:15 +0200

    :label: Add typescript to randomColor

---
+++ src/utils/randomColor.ts
@@ -0,0 +1,39 @@
+import { rgb255ToHex } from '../converter';
+import type { RGB1, RGB255, RGBA1, RGBA255 } from '../types/rgb';
+
+function getRandomRgb255Color({
+    transparency = false,
+    rgb1 = false
+} = {}): RGB255 | RGBA255 | RGB1 | RGBA1 {
+    const rgb: {
+        r: number | null;
+        g: number | null;
+        b: number | null;
+        a?: number | null;
+    } = {
+        r: null,
+        g: null,
+        b: null,
+    };
+
+    if (transparency) {
+        rgb.a = null;
+    }
+
+    for (let i = 0; i < Object.keys(rgb).length; i += 1) {
+        const component = Object.keys(rgb)[i] as 'r' | 'g' | 'b' | 'a';
+        rgb[component] = Math.random();
+
+        if (!rgb1 && component !== 'a') {
+            rgb[component] = Math.round((rgb[component] || 0) * 255);
+        }
+    }
+
+    return rgb as RGB255 | RGBA255 | RGB1 | RGBA1;
+}
+
+function getRandomHexColor(transparency = false): string | null {
+    return rgb255ToHex(getRandomRgb255Color({ transparency }));
+}
+
+export { getRandomHexColor, getRandomRgb255Color };
